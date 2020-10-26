5243957d3e48f6874163aa3710a72d942a1a39be
armandojsalazar <armando.salazar89@gmail.com>
2020-10-26 16:05:31 -0500

    feat: added not null default to name of album and UNSIGNED to sales

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(20),
-    name VARCHAR(20),
-    release_date INT(4),
-    sales FLOAT(8),
+    artist VARCHAR(20) NOT NULL,
+    name VARCHAR(50) NOT NULL DEFAULT 'Untitled',
+    release_date YEAR(4),
+    sales FLOAT UNSIGNED,
     genre VARCHAR(20),
     PRIMARY KEY(id)
 );
