d7d32d5f968ee6353be39c5f6b3e386eccd9c45b
armandojsalazar <armando.salazar89@gmail.com>
2020-10-26 15:06:57 -0500

    feat: completed the exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,12 @@
-DROP DATABASE IF EXISTS codeup_test_db;
-CREATE DATABASE codeup_test_db;
+
 USE codeup_test_db;
 
-DROP TABLE IF EXIST albums;
+DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(20),
     name VARCHAR(20),
-    release_date INT (4),
+    release_date INT(4),
     sales FLOAT(8),
     genre VARCHAR(20),
     PRIMARY KEY(id)
