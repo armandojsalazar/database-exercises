e9a6599f90da4a2ce1d2bdde99f2104d965bc263
armandojsalazar <armando.salazar89@gmail.com>
2020-10-27 11:24:17 -0500

    feat: added Select queries ot the select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,25 @@
 USE codeup_test_db;
 
-SELECT * FROM
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT year
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT name
+FROM album
+WHERE year = 1990;
+
+SELECT name
+FROM albums
+WHERE sales = <20.0;
+
+SELECT name
+FROM albums
+WHERE genre = 'Rock';
