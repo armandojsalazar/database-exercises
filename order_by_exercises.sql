1949eb8a36ea5367c85c31596c69894a979937f6
armandojsalazar <armando.salazar89@gmail.com>
2020-10-28 11:59:11 -0500

    feat: completed orderby exercise 6

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,12 +6,12 @@ FROM employees
 WHERE first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya'
-ORDER BY first_name, last_name;
+ORDER BY first_name DESC, last_name DESC;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
 
 SELECT *
 FROM employees
