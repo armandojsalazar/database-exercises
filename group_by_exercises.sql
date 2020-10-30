6cfe4cff67ce8bc6037d5bace552cfd77b4c3130
armandojsalazar <armando.salazar89@gmail.com>
2020-10-30 12:31:41 -0500

    feat: Completed question 3 for the exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,4 +1,13 @@
 USE employees;
 
+# In your script, use DISTINCT to find the unique titles in the titles table
 SELECT DISTINCT title
 FROM titles;
+
+
+#Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY.
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY last_name;
